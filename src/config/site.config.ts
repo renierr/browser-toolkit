6794d6f07dbc2869f6d02a295a5dc53befc9bbdf
renierr <renierr@users.noreply.github.com>
2025-12-18 23:59:05 +0100

    add own site config and change name of project to browser-toolkit

---
+++ src/config/site.config.ts
@@ -0,0 +1,21 @@
+import type { SiteConfig } from './types';
+
+/* copy to site.config.ts to customize */
+export const siteConfig = {
+  title: 'Browser Tools',
+  description: 'Collection of useful tools running only in Browser - with offline support',
+  logoPath: '/favicon.svg',
+  logoAlt: 'Browser Tools',
+  footerText: '© 2025 <span class="font-semibold text-brand">Browser Tools</span>',
+  showExamples: false,
+  toolSections: {
+    general: {
+      title: 'General',
+      description: 'General helper and Utilities.',
+    },
+    pdf: {
+      title: 'PDF Tools',
+      description: 'PDF-Tools, Viewers and Editors - 100% offline.',
+    },
+  },
+} satisfies SiteConfig;
