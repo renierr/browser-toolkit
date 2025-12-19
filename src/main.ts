25de2acbb6fc5090e0d4db3c1205640788ef9e9e
renierr <renierr@users.noreply.github.com>
2025-12-19 01:33:25 +0100

    globally prevent the pdf web viewer from opening the default pdf

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import type { CustomMainContext } from './js/types';
 export default function main(ctx: CustomMainContext) {
   console.log('Loaded tools:', ctx.tools.length);
 
-  // TODO some init logic
+  // globally prevent the pdf web viewer from opening the default pdf
+  document.addEventListener('webviewerloaded', async (evt: any) => {
+    evt?.detail?.source?.PDFViewerApplicationOptions.set('defaultUrl', '');
+  });
 }
