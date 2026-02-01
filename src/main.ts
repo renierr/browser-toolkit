5884972027ae9579c060615252208d82875ce2e8
renierr <renierr@users.noreply.github.com>
2026-02-01 16:18:20 +0100

    remove pdfjs dependency and editor tool
    remove other pdfjs helper stuff

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import type { CustomMainContext } from './js/types';
-import { onPdfViewerLoaded, setPdfViewerOptions, setupGlobalWebViewerDelegate } from './js/pdf-utils.ts';
 
 // noinspection JSUnusedGlobalSymbols
 export default function main(_: CustomMainContext) {
-  setupGlobalWebViewerDelegate();
-  // prevent the pdf web viewer from opening the default pdf
-  onPdfViewerLoaded((viewerWindow) => {
-    setPdfViewerOptions(viewerWindow, { defaultUrl: '', enableSignatureEditor: true });
-  });
 }
