b3f5dcbae0b9043f236cc06c02fe65a8fd4aa3ae
Renier <renierr@users.noreply.github.com>
2026-03-03 23:07:59 +0100

    fix glob pattern

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         enabled: true,
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico,wasm,onnx}', 'lib/imgly/**/*'],
+        globPatterns: ['**/*.{js,css,html,svg,png,ico,wasm,onnx}'],
         navigateFallbackDenylist: [/\.html($|\?)/],
         skipWaiting: true,
         clientsClaim: true,
